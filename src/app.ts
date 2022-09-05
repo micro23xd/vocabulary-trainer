7ef834c0b1809997847012bbb253940cf99868b8
micro23xd <dominik.bauer@bitvavo.com>
2022-09-05 19:39:00 +0200

    simpler import

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
 const prompt = require('prompt')
+import * as fs from 'node:fs';
 
 import { VocabularyList } from './vocabulary/vocabulary.class'
+import { Lesson } from "./vocabulary/vocabulary.types";
 import { config } from './config'
 
-import * as les1 from './lists/les1.json'
-import * as les2 from './lists/les2.json'
-import * as les3 from './lists/les3.json'
+
+const importLessons = async (): Promise<Lesson[]> => {
+    const files = fs.readdirSync(config.folder)
+    return files.map((file) => {
+        const content = fs.readFileSync(`${config.folder}/${file}`, 'utf8')
+        return JSON.parse(content) as Lesson
+    })
+}
 
 const main = async () => {
     prompt.start()
     const vocabularyList = new VocabularyList(prompt, config.askingLanguage)
-    vocabularyList.importLesson(les1)
-    vocabularyList.importLesson(les2)
-    vocabularyList.importLesson(les3)
+    const lists = await importLessons()
+    lists.forEach((list ) => vocabularyList.importLesson(list))
 
     await vocabularyList.runTrainer(config.rounds)
 }
--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,13 @@ import { Language } from './vocabulary/vocabulary.types'
 type Config = {
     rounds: number
     askingLanguage: Language
+    folder: string
 }
 
 const config = {
     askingLanguage: 'nl',
     rounds: 5,
+    folder: './src/lists',
 } as Config
 
 export { Config, config }
