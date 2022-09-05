38c8c6d75fdf6c961e4fa51ab5644ce274e2f22b
micro23xd <dominik.bauer@bitvavo.com>
2022-09-05 15:28:52 +0200

    lesson 3

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,18 @@ import { config } from './config'
 
 import * as les1Import from './lists/les1.json'
 import * as les2Import from './lists/les2.json'
+import * as les3Import from './lists/les3.json'
 
 let les1: Lesson = <Lesson>les1Import
 let les2: Lesson = <Lesson>les2Import
+let les3: Lesson = <Lesson>les3Import
 
 const main = async () => {
     prompt.start()
     const vocabularyList = new VocabularyList(prompt, config.askingLanguage)
     vocabularyList.importLesson(les1)
     vocabularyList.importLesson(les2)
+    vocabularyList.importLesson(les3)
 
     await vocabularyList.runTrainer(config.rounds)
 }
